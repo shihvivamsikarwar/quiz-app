a84c35e04328567921f291a8025f58f66a141216
Shivam Sikarwar <163384691+shihvivamsikarwar@users.noreply.github.com>
2025-04-13 06:32:36 +0530

    Initial commit

---
+++ src/test/java/io/github/shivamsikarwar/quizapp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shivamsikarwar.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
